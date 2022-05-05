fde13368e0ff12ea77591617e01f50adb0d05e80
ramiomarouayache <ramidnc14@gmail.com>
2022-05-05 20:23:06 +0100

    1.re-structure the code 2.add some admin pages

--- App.py
+++ App.py
@@ -8,22 +8,47 @@ bootstrap = Bootstrap(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('admin/404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('admin/500.html'), 500
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
+
+
+@app.route('/connecter')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/admin')
+def index_admin():
+    return render_template('admin/index.html')
+
+
+@app.route('/admin/transfer_interne')
+def transferInterne():
+    return render_template('admin/transfer_interne.html')
+
+
+@app.route('/admin/demande_details/<id_transfer>')
+def transferInterneDetails(id_transfer):
+    return render_template('admin/demande_details.html',id_transfer=id_transfer)
+
+
+@app.route('/admin/profile')
+def profile():
+    return render_template('admin/profile.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('admin/user.html', name=name)
 
 if __name__ == ("__main__"):
     app.run(debug=True)
